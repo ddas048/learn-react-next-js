855034db059b390d0bf9eb660aee5fb0ad37ad13
ddas042 <web.chinawholesale@gmail.com>
2023-03-07 03:48:28 +0600

    state

--- react.js
+++ react.js
@@ -5,11 +5,27 @@ function Header({ title }) {
 }
 
 function HomePage() {
+  React.useState();
+
+  const [likes, setLikes] = React.useState(0);
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
   return (
     <div>
       {/* Nesting the Header component */}
       <Header />
       <Header title="🔥 Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
     </div>
   );
 }
